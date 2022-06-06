c01faf6c43fbfe8ba4aa5629833f83e62513a0df
nat13alia <natalia13krawczyk@gmail.com>
2022-06-06 23:35:33 +0200

    Added class for calculating EF and intervals

---
+++ PersianMemo/UtilityClasses/FactorsCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersianMemo.UtilityClasses
+{
+    public class FactorsCalculator
+    {
+        public double CalculateEF(double oldEF, double q)
+        {
+            double newEF;
+            newEF = oldEF - 0.8 + 0.28 * q - 0.02 * q * q;
+            return newEF;
+        }
+
+        public double CalculateInterval(double oldInterval, double EF)
+        {
+            double newInterval;
+            newInterval = oldInterval * EF;
+            return newInterval;
+        }
+    }
+}
