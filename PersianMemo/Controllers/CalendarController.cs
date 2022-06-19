943e490d771aa13886c8d2d859dbd8ce1d50443d
nat13alia <natalia13krawczyk@gmail.com>
2022-06-19 22:07:43 +0200

    Improved design of Landing Page, Register, Login and _Layout views

---
+++ PersianMemo/Controllers/CalendarController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersianMemo.Controllers
+{
+    public class CalendarController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersianMemo/ViewModels/CalendarViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using PersianMemo.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersianMemo.ViewModels
+{
+    public class CalendarViewModel
+    {
+        public IEnumerable<Word> Words { get; set; }
+    }
+}
